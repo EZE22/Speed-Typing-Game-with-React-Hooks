1f1b0729fac6b414d012bf1f4b194c83cdfcf60c
Adam Ingram <everett.ingram22@gmail.com>
2020-04-26 10:28:44 -0400

    Basic structure for game

--- speed_typing_game/src/App.js
+++ speed_typing_game/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
 
+/**
+ * 1. <h1> title at the top
+ * 2. <textarea> for the box to type in
+ *      (tip: React normalizes <textarea /> to be more like <input />,
+ *      so it can be used as a self-closing element and uses the `value` property
+ *      to set its contents)
+ * 3. <h4> ti display the amount of time remaining
+ * 4. <button> to start the game
+ * 5. Another <h1> to display the word count
+ */
+
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the Speed Typing Game!</h1>
+    <div>
+        <h1>Speed Typing Game</h1>
+        <textarea value="Start Typing..."/>
+        <h4>Time Remaining</h4>
+        <button>Start</button>
+        <h1>Word Count</h1>
     </div>
   );
 }
