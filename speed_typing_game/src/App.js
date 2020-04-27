9b6088237608967a02274dd7b8e51717e1b267c7
Adam Ingram <everett.ingram22@gmail.com>
2020-04-27 16:30:34 -0400

    Added useState Hook to track state of textarea

--- speed_typing_game/src/App.js
+++ speed_typing_game/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [text, setText] = useState("Start Typing... ")
+
+    function handleTextChange(event){
+        const {value} = event.target
+        setText(value)
+    }
+
   return (
     <div>
         <h1>Speed Typing Game</h1>
-        <textarea value="Start Typing..."/>
+        <textarea value={text} onChange={handleTextChange}/>
         <h4>Time Remaining</h4>
         <button>Start</button>
         <h1>Word Count</h1>
