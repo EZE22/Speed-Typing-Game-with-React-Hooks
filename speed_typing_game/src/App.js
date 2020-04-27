3ab1089dbf4335ade439f1df903664d6e37affe4
Adam Ingram <everett.ingram22@gmail.com>
2020-04-27 16:58:02 -0400

    Updated Project Structure

--- speed_typing_game/src/App.js
+++ speed_typing_game/src/App.js
@@ -13,9 +13,9 @@ function App() {
     <div>
         <h1>Speed Typing Game</h1>
         <textarea value={text} onChange={handleTextChange}/>
-        <h4>Time Remaining</h4>
+        <h4>Time Remaining: ???</h4>
         <button>Start</button>
-        <h1>Word Count</h1>
+        <h1>Word Count: ???</h1>
     </div>
   );
 }
