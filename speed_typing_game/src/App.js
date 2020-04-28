259fa49e9c7c5cd4dee220f3fc05565cdd460c21
Adam Ingram <everett.ingram22@gmail.com>
2020-04-28 15:18:17 -0400

    calculate the number of separate words

--- speed_typing_game/src/App.js
+++ speed_typing_game/src/App.js
@@ -2,11 +2,15 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [text, setText] = useState("Start Typing... ")
+    const [text, setText] = useState("")
 
     function handleTextChange(event){
         const {value} = event.target
         setText(value)
+    }
+
+    function calculateWordCount(words) {
+        return words.split(" ").filter((word) => word !== "").length
     }
 
   return (
@@ -14,7 +18,7 @@ function App() {
         <h1>Speed Typing Game</h1>
         <textarea value={text} onChange={handleTextChange}/>
         <h4>Time Remaining: ???</h4>
-        <button>Start</button>
+        <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
         <h1>Word Count: ???</h1>
     </div>
   );
